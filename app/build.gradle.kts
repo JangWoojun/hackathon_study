bed2458c15c6361aa7f278460e282c59f388e9e8
JangWoojun <woojun0107@naver.com>
2024-01-05 15:58:50 +0900

    fix: compileSdk 33 오류 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.woojun.hackathonstudy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.woojun.hackathonstudy"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

9872d4b1d556b3dd7438a691680b11ed86c68256
JangWoojun <woojun0107@naver.com>
2024-01-10 21:17:33 +0900

    feat: 이미지 OCR 기능 추가

--- app/src/main/java/com/woojun/hackathonstudy/OCRActivity.kt
+++ app/src/main/java/com/woojun/hackathonstudy/OCRActivity.kt
@@ -1,11 +1,56 @@
 package com.woojun.hackathonstudy
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
+import com.woojun.hackathonstudy.databinding.ActivityOcractivityBinding
 
 class OCRActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOcractivityBinding
+
+    companion object CODE {
+        const val REQUEST_CODE = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ocractivity)
+        binding = ActivityOcractivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            selectImageButton.setOnClickListener {
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                startActivityForResult(intent, REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val imageUri: Uri? = data?.data
+            val imageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+            val image = InputImage.fromBitmap(imageBitmap, 0)
+
+            val recognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
+            recognizer.process(image)
+                .addOnSuccessListener { visionText ->
+                    Toast.makeText(this@OCRActivity, "${visionText.text}", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Log.d("확인", "에러 메시지 $e")
+                }
+
+
+        }
     }
 }

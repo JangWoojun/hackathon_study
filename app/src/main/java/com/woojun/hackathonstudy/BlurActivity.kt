81eeb475477907a276d74260de36b468ca8aaa2a
JangWoojun <woojun0107@naver.com>
2024-01-11 05:59:40 +0900

    feat: 버튼 클릭 시 블러 효과 적용

--- app/src/main/java/com/woojun/hackathonstudy/BlurActivity.kt
+++ app/src/main/java/com/woojun/hackathonstudy/BlurActivity.kt
@@ -1,11 +1,15 @@
 package com.woojun.hackathonstudy
 
+import android.graphics.BlurMaskFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.woojun.hackathonstudy.databinding.ActivityBlurBinding
 
 class BlurActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBlurBinding
+    private var isBlur = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +17,18 @@ class BlurActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
-
+            blurButton.setOnClickListener {
+                setBlurText(isBlur, it)
+                isBlur = !isBlur
+            }
         }
     }
+    private fun setBlurText(isBlur: Boolean, view: View) {
+        view as Button
+        view.setLayerType(View.LAYER_TYPE_SOFTWARE, null).apply {
+            if (isBlur) view.paint.maskFilter = BlurMaskFilter(16f, BlurMaskFilter.Blur.NORMAL)
+            else view.paint.maskFilter = null
+        }
+    }
+
 }

c93cad5a91d5beddde4b7387765395df04916112
JangWoojun <woojun0107@naver.com>
2024-01-11 05:19:51 +0900

    feat: binding 설정

--- app/src/main/java/com/woojun/hackathonstudy/BlurActivity.kt
+++ app/src/main/java/com/woojun/hackathonstudy/BlurActivity.kt
@@ -2,10 +2,18 @@ package com.woojun.hackathonstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woojun.hackathonstudy.databinding.ActivityBlurBinding
 
 class BlurActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBlurBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_blur)
+        binding = ActivityBlurBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+        }
     }
 }

7b2c5a83c850011d8b384cb0b936c3dad96b0b3d
JangWoojun <woojun0107@naver.com>
2024-01-12 20:58:30 +0900

    feat: OCR 결과 textView로 띄우게 변경

--- app/src/main/java/com/woojun/hackathonstudy/OCRActivity.kt
+++ app/src/main/java/com/woojun/hackathonstudy/OCRActivity.kt
@@ -7,21 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
-import com.woojun.hackathonstudy.databinding.ActivityOcractivityBinding
+import com.woojun.hackathonstudy.databinding.ActivityOcrBinding
 
 class OCRActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityOcractivityBinding
+    private lateinit var binding: ActivityOcrBinding
 
     companion object CODE {
         const val REQUEST_CODE = 1
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOcractivityBinding.inflate(layoutInflater)
+        binding = ActivityOcrBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.apply {
@@ -44,7 +44,10 @@ class OCRActivity : AppCompatActivity() {
             val recognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
             recognizer.process(image)
                 .addOnSuccessListener { visionText ->
-                    Toast.makeText(this@OCRActivity, "${visionText.text}", Toast.LENGTH_SHORT).show()
+                    binding.apply {
+                        textView.text = visionText.text
+                        selectImageButton.visibility = View.INVISIBLE
+                    }
                 }
                 .addOnFailureListener { e ->
                     Log.d("확인", "에러 메시지 $e")

3c64f1915cfd722aa2891c972c417054597a4f01
JangWoojun <woojun0107@naver.com>
2024-01-12 19:28:28 +0900

    feat: HWPActivity 추가

---
+++ app/src/main/java/com/woojun/hackathonstudy/HWPActivity.kt
@@ -0,0 +1,11 @@
+package com.woojun.hackathonstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HWPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hwpactivity)
+    }
+}
